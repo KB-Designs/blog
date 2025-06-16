e3619225098b9cdccf81fd85b1c40a2b8e334e82
Brian <bryoroba@gmail.com>
2025-06-16 15:55:49 +0300

    updates: blog app : authentication

--- web_blog/blog/urls.py
+++ web_blog/blog/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name='blog'
 
 
 urlpatterns=[
     path('', views.home, name='post_list'),
+    path('register/',views.register, name='register'),
+    path('login/', views.user_login, name='login'),
     path('about/', views.about, name='about'),
 ]
--- web_blog/blog/views.py
+++ web_blog/blog/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from  .models import Post
+from django.contrib.auth import authenticate, login
+from .forms import UserRegistrationForm, LoginForm
+
+
+
 
 def home(request):
     posts = Post.objects.all()  # Fetch all posts from the database
@@ -8,4 +14,41 @@ def home(request):
 def about(request):
     return render(request, 'blog/post/detail.html')
 
-# Create your views here.
+def user_login(request):
+    if request.method=='POST':
+        form=LoginForm(request.POST)
+        if form.is_valid():
+            cd=form.cleaned_data
+            user=authenticate(request,
+                              username=cd['username'],
+                              password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('autheticated'\
+                                        'successfully')
+                else:
+                    return HttpResponse('Diabled account')
+            else:
+                return HttpResponse('Invalid login')  
+    else:
+        form=LoginForm()
+    return render(request, 'blog/authentication/login.html', {'form':form})              
+    
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            # Set password properly
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            # Redirect to login page after successful registration
+            return redirect('login')  # Make sure you have a URL named 'login'
+        # If form is invalid, fall through to render with errors
+    else:
+        user_form = UserRegistrationForm()
+    
+    return render(request, 'blog/authentication/register.html', {'user_form': user_form})
+
